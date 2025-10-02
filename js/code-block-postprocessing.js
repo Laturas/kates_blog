ffd888ec9a3137aec41d052960d2ece886daca89
Laturas <laturaas@gmail.com>
2025-10-02 14:08:12 -0400

    code block postprocessing optimization

--- js/code-block-postprocessing.js
+++ js/code-block-postprocessing.js
@@ -5,31 +5,22 @@ code_blocks.forEach(code_block => {
     const lines = document.createElement('code-body');
     for (i = 0; i < count; i++) {
         const num = document.createElement('code-number');
-        const text_node = document.createTextNode(i);
-        num.appendChild(text_node);
+        num.appendChild(document.createTextNode(i));
         numbers.append(num);
+
         const new_line = code_block.children[i].cloneNode(true);
-        new_line.textContent = code_block.children[i].textContent;
         lines.append(new_line);
         lines.append(document.createElement('br'));
     }
-    code_block.prepend(numbers);
-    code_block.append(lines);
-    const old_lines = code_block.querySelectorAll(':scope > code-line');
-
-    old_lines.forEach(line => {
-        line.remove();
-    });
-
     const new_block = document.createElement('code-block');
-
-    new_block.innerHTML = code_block.innerHTML;
-
+    new_block.append(numbers);
+    new_block.append(lines);
     if (code_block.id) {
         new_block.id = code_block.id;
     }
     if (code_block.className) {
         new_block.className = code_block.className;
     }
+
     code_block.parentNode.replaceChild(new_block, code_block);
 });
